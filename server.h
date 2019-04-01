1a3372c90e9d0a4849e59b392c7bd2438091b415
aswindeler <49133581+aswindeler@users.noreply.github.com>
2019-03-31 20:24:21 -0500

    Add files via upload

---
+++ server.h
@@ -0,0 +1,53 @@
+//server.cpp (?)
+
+class Server()
+{
+public:
+	Server()
+	~Server()
+	std::vector<Client> list_of_client;
+	std::vector<Chatroom> list_of_chatroom;
+
+	void read_head(struct message_head) {
+		
+	}
+
+	void read_body(struct message_body) {
+
+	}
+
+	void add_client(Client client) {
+
+	}
+
+	void disconnect(Client client) {
+
+	}
+
+	void ban(Client client) {
+
+	}
+
+	void create_chatroom() {
+
+	}
+
+	void delete_chatroom(Chatroom chatroom) {
+
+	}
+
+	void save_state() {
+
+	}
+
+	void change_chatroom(string chatroom_name) {
+
+	}
+
+	void check_name(string client_name) {
+
+	}
+
+private:
+	int chatroom_size, number_of_clients;
+}
