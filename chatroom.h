45d347e043575a0618ceaecb54e501c9dba3fc3f
shivangi827 <35352776+shivangi827@users.noreply.github.com>
2019-03-31 20:16:27 -0500

    Add files via upload

---
+++ chatroom.h
@@ -0,0 +1,24 @@
+//Chatroom Header File
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include<sstream>
+#ifndef chatroom_H
+#define chatroom_H
+
+class chatroom_H
+{
+	public:
+	chatroom(std:: string name, Bool ispublic, int port);
+	join_chatroom();
+	set_public();
+	record_history();
+	private:
+
+
+
+
+}
+#endif
