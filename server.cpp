66902bfef226b08c7fd72d1040c73ac3cbb4689f
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-03-31 20:40:05 -0500

    Class file for server

---
+++ server.cpp
@@ -0,0 +1,45 @@
+#include "server.h"
+
+using namespace std;
+
+server:server(){};                                    // no constructor
+
+  void server::read_head(struct message_head) {
+		
+	}
+
+	void server::read_body(struct message_body) {
+
+	}
+
+	void server::add_client(Client client) {
+
+	}
+
+	void server::disconnect(Client client) {
+
+	}
+
+	void server::ban(Client client) {
+
+	}
+
+	void server::create_chatroom() {
+
+	}
+
+	void server::delete_chatroom(Chatroom chatroom) {
+
+	}
+
+	void server::save_state() {
+
+	}
+
+	void server::change_chatroom(string chatroom_name) {
+
+	}
+
+	void server::check_name(string client_name) {
+
+	}
