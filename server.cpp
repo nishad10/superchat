8113f3f8af53c30498483b8ec174e7bdbeb4196a
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-03-31 20:31:04 -0500

    Server class file

--- server.cpp
+++ server.cpp
@@ -2,52 +2,32 @@
 
 class Server()
 {
+	
+private:
+	int chatroom_size, number_of_clients;
 public:
 	Server()
 	~Server()
 	std::vector<Client> list_of_client;
 	std::vector<Chatroom> list_of_chatroom;
 
-	void read_head(struct message_head) {
-		
-	}
+	void read_head(struct message_head);
 
-	void read_body(struct message_body) {
+	void read_body(struct message_body);
 
-	}
+	void add_client(Client client);
 
-	void add_client(Client client) {
+	void disconnect(Client client);
 
-	}
+	void ban(Client client);
+	
+	void create_chatroom();
 
-	void disconnect(Client client) {
+	void delete_chatroom(Chatroom chatroom);
 
-	}
+	void save_state();
 
-	void ban(Client client) {
+	void change_chatroom(string chatroom_name);
 
-	}
-
-	void create_chatroom() {
-
-	}
-
-	void delete_chatroom(Chatroom chatroom) {
-
-	}
-
-	void save_state() {
-
-	}
-
-	void change_chatroom(string chatroom_name) {
-
-	}
-
-	void check_name(string client_name) {
-
-	}
-
-private:
-	int chatroom_size, number_of_clients;
+	void check_name(string client_name);
 }
