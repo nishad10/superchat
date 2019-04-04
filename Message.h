26b19e66923687201f9c51306c64f0cfe092a3c0
bshesh7 <44710882+bshesh7@users.noreply.github.com>
2019-04-04 09:41:38 -0500

    Add files via upload
    Message class and header file

---
+++ Message.h
@@ -0,0 +1,28 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include<iostream>
+#include <string>
+
+class CokeMachine{
+
+    public:
+        std::string encode(std::string head, std:: body);
+
+        std::string decode_head(std::string endc_str);
+
+        std::string decode_body(std::string endc_str);
+
+        void encrypt(std::string body);
+
+        void decrypt(std::string body);
+
+    private :
+        std::string head;
+        std::string body;
+        int length;
+
+
+};
+#endif
+
