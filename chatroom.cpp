17ed45757fed43a15b5dfc2da7cd95e5bd5f9a5e
shivangi827 <35352776+shivangi827@users.noreply.github.com>
2019-04-04 20:40:25 -0500

    Add files via upload

---
+++ chatroom.cpp
@@ -0,0 +1,58 @@
+	chatroom(std:: string name, Bool ispublic, int port)
+	{
+	
+	
+	}
+	join_chatroom()
+	{
+
+	}
+	set_public()
+	{
+		
+	}
+	record_history(Message myMessage)
+	{
+		
+	}
+	bool get_is_empty()
+	{
+		
+	}
+	bool get_is_lobby()
+	{
+		
+	}
+	bool get_is_public()
+	{
+		
+	}
+	set_is_empty(bool is_empty)
+	{
+		
+	}
+	set_is_lobby(bool Lobby)
+	{
+		
+	}
+	set_is_public(bool is_public)
+	{
+		
+	}
+	std:: string display_previous(chatroom Chatroom)
+	{
+		
+	}
+	std:: string get_name()
+	{
+		
+	}
+	set_name(std::string name)
+	{
+		
+	}
+	int get_port()
+	{
+		
+	}
+	
--- chatroom.h
+++ chatroom.h
@@ -5,18 +5,39 @@
 #include <string>
 #include <fstream>
 #include<sstream>
+#include "Client.h"
+#include "Message.h"
 #ifndef chatroom_H
 #define chatroom_H
 
-class chatroom_H
+class chatroom
 {
 	public:
 	chatroom(std:: string name, Bool ispublic, int port);
 	join_chatroom();
 	set_public();
-	record_history();
+	record_history(Message myMessage);
+	bool get_is_empty(); 
+	bool get_is_lobby();
+	bool get_is_public();
+	set_is_empty(bool is_empty);
+	set_is_lobby(bool Lobby);
+	set_is_public(bool is_public);
+	std:: string display_previous(chatroom Chatroom);
+	std:: string get_name();
+	set_name(std::string name);
+	int get_port();
+	
+	vector<Client> Client_list;
+	vector<std::string> message_list;
+	
 	private:
-
+	
+	bool is_public;
+	bool is_empty;
+	bool Lobby;
+	std:: string name;
+	int port1;
 
 
 
