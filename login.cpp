7c562ad5b7e46a784e8e074c190969038e46b3a5
shivangi827 <35352776+shivangi827@users.noreply.github.com>
2019-04-17 11:25:46 -0500

    Add files via upload

--- login.cpp
+++ login.cpp
@@ -10,20 +10,42 @@
 using namespace std;
 
 /*
-int CheckName
+int CheckName (vector <string> Name, string name, vector <string> Ban)
 {
-	for ()
-	if(Names[i]==name)
+	for (auto i: Names)
 	{
+		if(Names[i]==name)
+		{
+			mvprintw((row/2)+3,((col- 20)/2),"Sorry! Name already taken");
+					getch();
+					flag=false;
+					wrefresh(Outside);
+					int wclear(WINDOW *Outside);
+					//wrefresh(Outside);
+					//refresh();
+					endwin();
+					
 		return 2;
-	}
-	else if (Ban[i]==name)
-	{
+		break;
+		}
+		else if (Ban[i]==name)
+		{
+			mvprintw((row/2)+3,((col- 20)/2),"Sorry! You can't enter");
+					getch();
+					flag=false;
+					wrefresh(Outside);
+					int wclear(WINDOW *Outside);
+					//wrefresh(Outside);
+					//refresh();
+					endwin();
+					
 		return 1;
-	}
-	else
-	{
-		return 0;
+		break;
+		}
+		else
+		{
+			return 0;
+		}
 	}
 }
 */
@@ -33,29 +55,28 @@ int main()
 	 char getInput []="Pick a cool name: ";		/* message to be appeared on the screen */
 	 char str[25];
 	 vector <string> Names ;
-	 
 	 int row,col;				/* to store the number of rows and *
 						 * the number of colums of the screen */
 	 initscr();				/* start the curses mode */
-	 bool flag= true;
 			/* get the number of rows and columns */
 	 bool flagInput=true;
 		while(flagInput)
 		{
-			
+			 bool flag= true;
 			getmaxyx(stdscr,row,col);
 			 WINDOW * Outside = newwin(10, 55,(row/2)-5,((col- 20)/2)-5);
 			 refresh();
-			 box(Outside, 3, 3);
+			 box(Outside, 0, 0);
 			mvprintw(row/2,(col- 20)/2,"%s",getInput);
 			wrefresh(Outside);
 			getstr(str);
 			//wrefresh(Outside);
 			string name(str);
 				
-			for (int i=0;i<Names.size();i++)
+			//for (int i=0;i<Names.size();i++)
+			for (auto i: Names)
 			{
-				if (Names[i] == name)
+				if (i == name)
 				{
 					mvprintw((row/2)+3,((col- 20)/2),"Sorry! Name already taken");
 					getch();
